c8cf6cfbe937793bfdddc491f2b0679e10769545
Sahil-Thakrar <Sahil-Thakrar@github.com>
2021-10-22 18:06:31 +0530

    Error checking added at every step

--- Producer Consumer Using Threads/Producer_Consumer.c
+++ Producer Consumer Using Threads/Producer_Consumer.c
@@ -10,15 +10,24 @@ static char BUFFER[BufferSize];
 static pthread_cond_t Buffer_Full=PTHREAD_COND_INITIALIZER;
 static pthread_cond_t Buffer_Empty=PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+int err;
 
 static void *Producer()
 {
     while(1)
     {
-        pthread_mutex_lock(&mutex);
+        err=pthread_mutex_lock(&mutex);
+        if(err!=0)
+        {
+        	perror("Mutex Lock of producer Failed ");
+        }
         while(BufferIndex == 5)
         {
-        	pthread_cond_wait(&Buffer_Empty,&mutex);
+        	err=pthread_cond_wait(&Buffer_Empty,&mutex);
+        	if(err!=0)
+        	{
+        		perror("Conditional waiting for Empty Signal got failed ");
+        	}
         }
         for(int i=0;i<BufferSize;i++)
         {
@@ -29,8 +38,16 @@ static void *Producer()
         }
         printf("Buffer is full now\n");
         sleep(2);
-        pthread_mutex_unlock(&mutex);
-        pthread_cond_signal(&Buffer_Full);
+        err=pthread_mutex_unlock(&mutex);
+        if(err!=0)
+        {
+        	perror("Mutex Unlock of producer Failed ");
+        }
+        err=pthread_cond_signal(&Buffer_Full);
+        if(err!=0)
+        {
+        	perror("Signalling Full got an error ");
+        }
     }
     return NULL;
 }
@@ -38,10 +55,18 @@ static void *Consumer()
 {
     while(1)
     {
-        pthread_mutex_lock(&mutex);
+        err=pthread_mutex_lock(&mutex);
+        if(err!=0)
+        {
+        	perror("Mutex Lock of Consumer Failed ");
+        }
         while(BufferIndex == 0)
         {
-        	pthread_cond_wait(&Buffer_Full,&mutex);
+        	err=pthread_cond_wait(&Buffer_Full,&mutex);
+        	if(err!=0)
+        	{
+        		perror("Conditional waiting for Full Signal got failed ");
+        	}
         }
         for(int i=0;i<BufferSize;i++)
         {
@@ -50,17 +75,41 @@ static void *Consumer()
         }
         printf("Buffer is empty now\n");
         sleep(2);
-        pthread_mutex_unlock(&mutex);
-        pthread_cond_signal(&Buffer_Empty);
+        err=pthread_mutex_unlock(&mutex);
+        if(err!=0)
+        {
+        	perror("Mutex Unlock of consumer Failed ");
+        }
+        err=pthread_cond_signal(&Buffer_Empty);
+        if(err!=0)
+        {
+        	perror("Signalling of Empty got failed ");
+        }
     }
     return NULL;
 }
 int main()
 {
     pthread_t pro,con;
-    pthread_create(&pro,NULL,Producer,NULL);
-    pthread_create(&con,NULL,Consumer,NULL);
-    pthread_join(pro,NULL);
-    pthread_join(con,NULL);
+    err=pthread_create(&pro,NULL,Producer,NULL);
+    if(err!=0)
+    {
+        perror("Creation of Producer Thread Failed ");
+    }
+    err=pthread_create(&con,NULL,Consumer,NULL);
+    if(err!=0)
+    {
+        perror("Creation of Consumer Thread Failed ");
+    }
+    err=pthread_join(pro,NULL);
+    if(err!=0)
+    {
+        perror("Joining of Producer Thread Failed ");
+    }
+    err=pthread_join(con,NULL);
+    if(err!=0)
+    {
+        perror("Joining of Consumer Thread Failed ");
+    }
     return 0;
 }
